8f2df90b2f6ea3078012d737a766343388c1ad46
SHAHZAD36 <vengeance83836@gmail.com>
2026-05-16 11:09:14 +0500

    Update purchase_repository.dart

--- lib/features/purchases/domain/repositories/purchase_repository.dart
+++ lib/features/purchases/domain/repositories/purchase_repository.dart
@@ -1,4 +1,4 @@
-import '../../../data/models/purchase_model.dart';
+import '../../models/purchase_model.dart';
 abstract class PurchaseRepository {
   Future<List<PurchaseModel>> getPurchases();
   Future<int> addPurchase(PurchaseModel p);
